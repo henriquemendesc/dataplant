e337231d7ca106119612a6e3b5a3d6d044d97327
Henrique Mendes <henriiimendesc@gmail.com>
2018-05-27 14:40:34 -0300

    add

---
+++ ParseStarterProject/build/generated/source/apt/debug/com/parse/starter/BR.java
@@ -0,0 +1,5 @@
+package com.parse.starter;
+
+public class BR {
+        public static final int _all = 0;
+}
---
+++ ParseStarterProject/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="4642a99e-1c28-4f40-bef5-7799281b0bc5")
+public class DataBindingInfo {}
