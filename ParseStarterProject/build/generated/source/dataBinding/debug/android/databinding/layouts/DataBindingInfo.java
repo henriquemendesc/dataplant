a50407707d4c6282655fbcf9fd5a7880b7055ceb
Henrique Mendes <henrique@malalai.com.br>
2019-12-18 15:31:20 -0300

    alteracao gradle

--- ParseStarterProject/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="b1df42ed-4dd7-4111-89d9-b593e9087d23")
-public class DataBindingInfo {}
